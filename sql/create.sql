7bb1b34ccb51d70a242276bd86088d4700ce51bf
NisseCR <nissehermsen@gmail.com>
2024-09-18 14:45:18 +0200

    queries

--- sql/create.sql
+++ sql/create.sql
@@ -1,6 +1,59 @@
-drop table if exists assignment_1_test;
+drop table if exists customer;
+drop table if exists branch;
+drop table if exists account;
+drop table if exists loan;
+drop table if exists employee;
 
-create table assignment_1_test (
-    id integer primary key autoincrement,
-    name varchar(20)
+
+-- Create tables.
+create table customer (
+    customer_id integer not null primary key autoincrement,
+    street varchar(20) not null,
+    house_number varchar(20) not null, -- korea exists
+    city varchar(20) not null,
+    country varchar(20) not null,
+    first_name varchar(20) not null,
+    surname varchar(20) not null
 );
+
+create table branch (
+    branch_id integer not null primary key autoincrement, -- this is the key, not name, so change loan and employee. we smort
+    branch_name varchar(20) not null unique,
+    street varchar(20) not null,
+    building_number varchar(20) not null, -- korea exists
+    city varchar(20) not null,
+    phone_number varchar(20) unique -- leading zero!! and not not null because new branch setup b4 contract or elders have only physical place.
+);
+
+create table account (
+    account_id integer not null primary key autoincrement,
+    customer_id integer not null,
+    balance float not null,
+    branch_id integer not null,
+    foreign key (customer_id) references customer,
+    foreign key (branch_id) references branch
+);
+
+create table loan (
+    loan_id integer not null primary key autoincrement,
+    branch_id integer not null,
+    customer_id integer not null,
+    amount float not null,
+    currency varchar(20),
+    foreign key (branch_id) references branch,
+    foreign key (customer_id) references customer
+);
+
+create table employee (
+    employee_id integer not null primary key autoincrement,
+    branch_id integer not null,
+    first_name varchar(20) not null,
+    surname varchar(20) not null,
+    salary float not null, --include volunteers or not being paid ppl
+    role varchar(20) not null,
+    foreign key (branch_id) references branch,
+    check (salary >= 0) -- imagine paying to work
+);
+
+-- Seed database.
+-- todo
--- sql/query.sql
+++ sql/query.sql
@@ -1,2 +1,12 @@
-insert into assignment_1_test (name)
-values ("Alias");
+select
+    c.customer_id,
+    c.first_name,
+    c.surname,
+    sum(a.balance) as sum_balance
+from (
+    select *
+    from customer
+    where country = "Vatican City"
+) as c
+left join account as a on c.customer_id = a.customer_id
+group by c.customer_id
